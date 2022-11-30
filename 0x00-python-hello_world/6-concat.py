b7195a7bbd053bdeab553999b68644ffc79e6a40
Oluwatomi <ajakatomi@gmail.com>
2022-11-30 20:40:31 +0100

    Python

--- 0x00-python-hello_world/6-concat.py
+++ 0x00-python-hello_world/6-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-str1 = "Holberton"
+str1 = "Holber"
 str2 = "School"
-str3 = str1 + ' ' + str2
-print(f"Welcome to {str3}!")
+str1 = str1 + ' ' + str2
+print(f"Welcome to {str1}!")
 
 
