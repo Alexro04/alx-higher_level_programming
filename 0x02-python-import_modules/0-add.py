8c7df2acb6ceb77719a15934aac018ee4cceb548
Oluwatomi <ajakatomi@gmail.com>
2022-12-02 06:33:56 -0800

    Python

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from add_0 import add
 
-a = 1
-b = 2
-print(f"{a} + {b} = {add(a, b)}")
+def add(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print(add(a, b))
+
+
