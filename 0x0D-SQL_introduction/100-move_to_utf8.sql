80959207e7da569727c79abb8522e5c87dcff244
Oluwatomi <ajakatomi@gmail.com>
2023-05-23 23:09:26 +0100

    SQL advanced introduction

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,7 +1,7 @@
 -- convert database to UTF8mb4
 ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
---convert table to UTF8mb4
+-- convert table to UTF8mb4
 ALTER TABLE `first_table` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- convert field 'name' in first_table to UTF8mb4
