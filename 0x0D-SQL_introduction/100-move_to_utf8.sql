b49c787bab59124ebc6531995d7282561aa49313
Oluwatomi <ajakatomi@gmail.com>
2023-05-23 23:17:42 +0100

    sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,2 +1,5 @@
--- convert database to UTF8mb4
-ALTER DATABASE `hbtn_0c_0` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- convert entire database to UTF8mb4
+USE `hbtn_0c_0`;
+ALTER TABLE `first_table` 
+CONVERT TO CHARACTER SET utf8mb4 
+COLLATE utf8mb4_unicode_ci;
