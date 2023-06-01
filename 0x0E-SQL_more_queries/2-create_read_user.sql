454b8d82dc1fe660b2ae1f74a4cef89983302064
Oluwatomi <ajakatomi@gmail.com>
2023-06-01 12:52:10 +0100

    SQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,11 +1,9 @@
 -- Creates user and database and grants specific priviledges
---CREATES USER user_0d_2
-DROP USER IF EXISTS 'user_0d_2'@'localhost';
-CREATE USER 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
 --CREATE DATABASE hbtn_0d_2
-DROP DATABASE IF EXISTS 'hbtn_0d_2';
-CREATE DATABASE 'hbtn_0d_2';
+CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
+
+--CREATES USER user_0d_2
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
 
 --Grant SELECT priviledge to 'hbtn_0d_2'
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+GRANT SELECT ON 'hbtn_0d_2'. * TO 'user_0d_2'@'localhost';
