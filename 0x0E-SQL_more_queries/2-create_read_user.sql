4abf677b65f0e89777b0d1a0c74138658988b5cc
Oluwatomi <ajakatomi@gmail.com>
2023-06-01 12:56:22 +0100

    SQL

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,9 +1,7 @@
--- Creates user and database and grants specific priviledges
---CREATE DATABASE hbtn_0d_2
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_2';
-
---CREATES USER user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-
---Grant SELECT priviledge to 'hbtn_0d_2'
-GRANT SELECT ON 'hbtn_0d_2'.* TO 'user_0d_2'@'localhost';
+-- creates the database hbtn_0d_2 and the user user_0d_2
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- creates a user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
