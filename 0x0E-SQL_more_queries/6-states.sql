2610e130b69ceff5f1f77ae913855809b1c3b52c
Oluwatomi <ajakatomi@gmail.com>
2023-06-02 16:29:01 +0100

    SQL more quaries

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,11 +1,7 @@
--- creates database and table in the database
--- creates database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
--- use database
-USE 'hbtn_0d_usa';
--- create a table
-CREATE TABLE IF NOT EXISTS 'states'(
-    id INT NOT NULL UNIQUE AUTO_INCREMENT, 
-    name VARCHAR(256) NOT NULL,
-    PRIMARY KEY(id)
-);
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,12 +1,13 @@
--- CREATES THE DATABASE hbtn_0d_usa
--- creates database
-CREATE DATABASE IF NOT EXISTS 'hbtn_0d_usa';
--- use database
-USE 'hbtn_0d_usa';
--- creates table
-CREATE TABLE IF NOT EXISTS 'cities'(
-    id INT AUTO_INCREMENT PRIMARY KEY,
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (
+    id INT UNIQUE AUTO_INCREMENT NOT NULL,
     state_id INT NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES states(id),
-    name VARCHAR(256)
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY(id),
+    FOREIGN KEY(state_id) REFERENCES states(id)
 );
