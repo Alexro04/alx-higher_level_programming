795bafde0b2fbc7b1cb609c6a1cf080ef6e22cea
Oluwatomi <ajakatomi@gmail.com>
2023-06-05 16:38:32 +0100

    Javascript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 
-let arr = ["C is fun", "Python is cool", "Javascript is amazing"]
+// a script that prints 3 lines
+const arr = ["C is fun", "Python is cool", "Javascript is amazing"];
+
 for (i=0; i<arr.length; i++){
-    console.log(arr[i])
-}
+    console.log(arr[i]);
+};
