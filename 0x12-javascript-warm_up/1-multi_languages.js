5f14b98ac63114fe2957c719cfa22c93e5a1c55b
Oluwatomi <ajakatomi@gmail.com>
2023-06-07 16:42:23 +0100

    Javascript

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-
-let first = "C is fun"
-let second = "Python is cool"
-let third = "Javascript is amazing"
-
-console.log(first + '\n' + second + '\n' + third)
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
