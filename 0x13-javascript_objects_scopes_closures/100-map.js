188a232944843fe3bf1573d1a222588e005ef85d
Oluwatomi <ajakatomi@gmail.com>
2023-06-19 23:17:52 +0100

    Javascript

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('101-data.js');
+const list = require('./101-data.js').list;
 const newList = list.map((element, index) => element * index);
 console.log(list);
 console.log(newList);
--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,25 +1,6 @@
 #!/usr/bin/node
-
 const fs = require('fs');
 
-function concatFiles (file1, file2, destination) {
-  try {
-    const content1 = fs.readFileSync(file1, 'utf8');
-    const content2 = fs.readFileSync(file2, 'utf8');
-    const concatenatedContent = content1 + content2;
-    fs.writeFileSync(destination, concatenatedContent);
-    console.log('Files concatenated successfully.');
-  } catch (err) {
-    console.error(`Error: ${err.message}`);
-  }
-}
-
-// Check if all three file paths are provided as command line arguments
-if (process.argv.length !== 5) {
-  console.log('Usage: node concat_files.js <file1> <file2> <destination>');
-} else {
-  const file1 = process.argv[2];
-  const file2 = process.argv[3];
-  const destination = process.argv[4];
-  concatFiles(file1, file2, destination);
-}
+const fileA = fs.readFileSync(process.argv[2]).toString();
+const fileB = fs.readFileSync(process.argv[3]).toString();
+fs.writeFileSync(process.argv[4], fileA + fileB);
